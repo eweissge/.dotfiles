87d191eb10ebeab2bf9bb4b93bf1bc27b0e80d35
eweissge <eweissge@gmail.com>
2019-09-13 11:49:31 -0400

    Added shell color library

---
+++ lib/lib.colors.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+NC='\033[0m'
+BLACK='\033[0;30m';
+DARKGRAY='\033[1;30m';
+RED='\033[0;31m';	
+LIGHTRED='\033[1;31m';
+GREEN='\033[0;32m';	
+LIGHTGREEN='\033[1;32m';
+BROWN='\033[0;33m';	
+YELLOW='\033[1;33m';
+BLUE='\033[0;34m';	
+LIGHTBLUE='\033[1;34m';
+PURPLE='\033[0;35m';	
+LIGHTPURPLE='\033[1;35m';
+CYAN='\033[0;36m';	
+LIGHTCYAN='\033[1;36m';
+LIGHTGRAY='\033[0;37m';	
+WHITE='\033[1;37m';
+
+# Black	COLOR_BLACK	0,0,0
+# Red
+
+
+#RED=`tput setaf 1`
+#GREEN=`tput setaf 2`
+#YELLOW=`tput setaf 3`
+#BLUE=`tput setaf 4`
+#MAGENTA=`tput setaf 5`
+#CYAN=`tput setaf 6`
+#WHITE=`tput setaf 7`
+#NC=`tput sgr0`
+
+
+
+#echo -e "${DARKGREY}T${RED}E${LIGHTRED}S${GREEN}T${LIGHTGREEN}I${BROWN}N${YELLOW}G ${BLUE}C${LIGHTBLUE}O${PURPLE}L${LIGHTPURPLE}O${CYAN}R${LIGHTCYAN}S${NC}"
